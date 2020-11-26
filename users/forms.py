c9dec454c0315998789154816f48feeffe0a7bd6
Marcos Souza <mrcsz.m@gmail.com>
2020-11-25 21:49:11 -0300

    ajustes na estrutura

--- users/forms.py
+++ users/forms.py
@@ -6,27 +6,27 @@ from django.contrib.auth import authenticate, login, logout, update_session_auth
 
 
 class RegistrationForm(UserCreationForm):
-    username =  forms.CharField(max_length=150)
+    username = forms.CharField(max_length=150)
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=150)
     email = forms.EmailField(max_length=254)
 
     class Meta:
         model = User
-        fields = ("username", "first_name", "last_name", "email","password1","password2")
+        fields = ("username", "first_name", "last_name", "email", "password1", "password2")
 
     def clean_username(self):
-            username = self.cleaned_data['username']
-            if User.objects.exclude(pk=self.instance.pk).filter(username=username).exists():
-                raise forms.ValidationError(u'Username "%s" is already in use.' % username)
-            return username
+        username = self.cleaned_data['username']
+        if User.objects.exclude(pk=self.instance.pk).filter(username=username).exists():
+            raise forms.ValidationError(u'Username "%s" is already in use.' % username)
+        return username
 
     def clean_email(self):
-            email = self.cleaned_data.get('email')
-            username = self.cleaned_data.get('username')
-            if email and User.objects.filter(email=email).exclude(username=username).exists():
-                raise forms.ValidationError('Email addresses must be unique.')
-            return email
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            raise forms.ValidationError('Email addresses must be unique.')
+        return email
 
     def save(self, commit=True):
         user = super(RegistrationForm, self).save(commit=False)
@@ -40,5 +40,3 @@ class RegistrationForm(UserCreationForm):
         if commit:
             user.save()
         return user
-
-
